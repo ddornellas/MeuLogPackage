1b9d2bb3fe08f1327e57166dbe3bbd89789907df
Danton Dornellas <dantondornellas@gmail.com>
2023-07-22 11:51:37 +0100

    First commit

--- Sources/MeuLogPackage/MeuLogPackage.swift
+++
@@ -1,6 +0,0 @@
-public struct MeuLogPackage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/MeuLogPackageTests/MeuLogPackageTests.swift
+++ Tests/MeuLogPackageTests/MeuLogPackageTests.swift
@@ -6,6 +6,6 @@ final class MeuLogPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MeuLogPackage().text, "Hello, World!")
+        //XCTAssertEqual(MeuLogPackage().text, "Hello, World!")
     }
 }
